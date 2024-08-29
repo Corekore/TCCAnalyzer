98e06ce53e8723b057c722f1e12fee9b25440ba5
corekore <corekore@skinny.local>
2024-08-29 18:08:32 +0300

    get clients/services from root db; fetch all clients/services from root and user dbs

---
+++ query_wrapper.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python3
+import os
+import sys
+
+# global vars
+rootDBFile = "/Library/Application\ Support/com.apple.TCC/TCC.db "
+userDBFile = "~/Library/Application\ Support/com.apple.TCC/TCC.db "
+
+def removeDuplicates():
+  os.chdir("output/")
+  for file in os.listdir(): 
+    print(f"Removing duplicates in {file}")
+    newFilename = file.replace("_raw", "") 
+    os.system(f"sort {file} | uniq > {newFilename}") 
+    os.remove(file)
+
+def fetchAllInfo():
+  if not os.path.isdir("output"):
+    os.mkdir("output")
+
+  print("Getting all services")
+  command = "\"select service from access;\""
+  os.system("sqlite3 " + rootDBFile + command + " | tee output/root_services_raw output/available_services_raw > /dev/null")
+  os.system("sqlite3 " + userDBFile + command + " | tee output/user_services_raw >> output/available_services_raw")
+
+  print("Getting all clients")
+  command = "\"select client from access;\""
+  os.system("sqlite3 " + rootDBFile + command + " | tee output/root_clients_raw output/available_clients_raw > /dev/null")
+  os.system("sqlite3 " + userDBFile + command + " | tee output/user_clients_raw >> output/available_clients_raw")
+
+  # remove duplicates
+  removeDuplicates()
+
+def getClients(service):
+  command = f"\"select client from access where service = '{service}';\""
+  os.system("sqlite3 " + rootDBFile + " " + command)
+
+def getServices(client):
+  command = f"\"select service from access where client = '{client}';\""
+  os.system("sqlite3 " + rootDBFile + " " + command)
+
+def main():
+  if len(sys.argv) == 1:
+    print("Fetching information from both root TCC.db and user TCC.db")
+    fetchAllInfo()
+  elif sys.argv[1] == "-h":
+    print("Usage: ./query_wrapper.py (-h help) (-s <service_name>) (-c <client_name>)"
+          "Example: ./query_wrapper.py -s kTCCServiceSystemPolicyAllFiles"
+          "         ./query_wrapper.py -c com.apple.Terminal")
+  elif sys.argv[1] == "-c":
+    print("Getting all services used by client: " + sys.argv[2])
+    getServices(sys.argv[2])
+  elif sys.argv[1] == "-s":
+    print("Getting all clients that use service: " + sys.argv[2])
+    getClients(sys.argv[2])
+
+if __name__ == "__main__":
+  main()
