3ce75276728d05e2ebfe0b296ddf4e8404ee6241
Corekore <cristian.radu00@yahoo.com>
2025-06-03 10:14:52 -0700

    added entitlement searcher

---
+++ get_entitlements.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+appPath="$1"
+
+echo "[*] Step 0"
+echo "[*] Getting entitlements for ${appPath}"
+codesign -d --ent - ${appPath}
+
+echo "[*] Step 1"
+if [ -d "${appPath}/Contents/Frameworks" ]; then
+  frameworksPath="${appPath}/Contents/Frameworks"
+  for dir in $(ls ${frameworksPath} | grep -e ".app"); do
+    echo "[*] Getting entitlements for ${frameworksPath}/${dir}"
+    codesign -d --ent - "${frameworksPath}/${dir}"
+  done
+fi
+
+echo "[*] Step 2"
+if [ -d "${appPath}/Contents/PlugIns" ]; then
+  pluginPath="${appPath}/Contents/PlugIns/"
+  for dir in $(ls ${pluginPath} | grep -e ".app"); do
+    echo "[*] Getting entitlements for ${pluginPath}/${dir}"
+    codesign -d --ent - "${pluginPath}/${dir}"
+  done
+fi
+
+echo "[*] Step 3"
+if [ -d "${appPath}/Contents/Extensions" ]; then
+  extensionsPath="${appPath}/Contents/Extensions"
+  for dir in $(ls ${extensionsPath} | grep -e ".app"); do
+    echo "[*] Getting entitlements for ${extensionsPath}/${dir}"
+    codesign -d --ent - "${extensionsPath}/${dir}"
+  done
+fi
+
+echo "[*] Step 4"
+if [ -d "${appPath}/Contents/Library" ]; then
+  echo "[?] Library dir spotted. Search for apps by hand." 
+fi
