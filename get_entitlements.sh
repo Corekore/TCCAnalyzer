04c421221d3dd62fbacdce602d786a910b0b278a
Corekore <cristian.radu00@yahoo.com>
2025-06-04 14:03:14 -0700

    added option to get entitlements from relevant sources

--- get_entitlements.sh
+++ get_entitlements.sh
@@ -3,40 +3,37 @@
 appPath="$1"
 
 usage() {
-  echo "Usage: ./get_entitlements.sh <path/to/Application.app>"
-  echo "Usage: ./get_entitlements.sh -t <path/to/Application.app>"
+  echo "Usage: ./get_entitlements.sh <path/to/Application.{app|appex|bundle}>"
+  echo "Usage: ./get_entitlements.sh -t <path/to/Application.{app|appex|bundle}>"
 }
 
 getEntitlements() {
   echo "[*] Step 0"
   echo "[*] Getting entitlements for ${appPath}"
-  codesign -d --ent - ${appPath}
+  codesign -d --ent - "${appPath}"
 
   echo "[*] Step 1"
   if [ -d "${appPath}Contents/Frameworks" ]; then
-    frameworksPath="${appPath}Contents/Frameworks"
-    for dir in $(ls ${frameworksPath} | grep -e ".app"); do
-      echo "[*] Getting entitlements for ${frameworksPath}/${dir}"
-      codesign -d --ent - "${frameworksPath}/${dir}"
-    done
+    while IFS= read -r -d '' dir; do
+      echo "[*] Getting entitlements for ${dir}"
+      codesign -d --ent - "${dir}"
+    done < <(find "${appPath}Contents/Frameworks" -maxdepth 1 -iname "*.app" -print0)
   fi
 
   echo "[*] Step 2"
   if [ -d "${appPath}Contents/PlugIns" ]; then
-    pluginPath="${appPath}Contents/PlugIns"
-    for dir in $(ls ${pluginPath} | grep -e ".app"); do
-      echo "[*] Getting entitlements for ${pluginPath}/${dir}"
-      codesign -d --ent - "${pluginPath}/${dir}"
-    done
+    while IFS= read -r -d '' dir; do
+      echo "[*] Getting entitlements for ${dir}"
+      codesign -d --ent - "${dir}"
+    done < <(find "${appPath}Contents/PlugIns" -maxdepth 1 -iname "*.app" -print0)
   fi
 
   echo "[*] Step 3"
   if [ -d "${appPath}Contents/Extensions" ]; then
-    extensionsPath="${appPath}Contents/Extensions"
-    for dir in $(ls ${extensionsPath} | grep -e ".app"); do
-      echo "[*] Getting entitlements for ${extensionsPath}/${dir}"
-      codesign -d --ent - "${extensionsPath}/${dir}"
-    done
+    while IFS= read -r -d '' dir; do
+      echo "[*] Getting entitlements for ${dir}"
+      codesign -d --ent - "${dir}"
+    done < <(find "${appPath}Contents/Extensions" -maxdepth 1 -iname "*.app" -print0)
   fi
 
   echo "[*] Step 4"
@@ -45,13 +42,37 @@ getEntitlements() {
   fi
 }
 
-while getopts 'th' opt; do
+getCommon() {
+  SCRIPT_PATH="$(cd "$(dirname "${BASH_SOURCE[0]}")" &> /dev/null && pwd)"
+  if [ ! -d "${SCRIPT_PATH}/ent_output" ]; then
+    mkdir "${SCRIPT_PATH}/ent_output"
+  fi
+
+  # common paths to search for apps?
+  declare -a commonPaths=("/Applications"
+                          "/System/Applications"
+                          "/System/Library/Frameworks"
+                          "/System/Library/PrivateFrameworks"
+                         )
+
+  # check each relevant path in turn
+  for path in "${commonPaths[@]}"; do
+    while IFS= read -r -d '' file; do
+      # change the path of the app bundle to be analyzed by getEntitlements
+      appPath="${file}"
+      getEntitlements >> "${SCRIPT_PATH}/ent_output/commonApplications"
+    done < <(find "${path}" -maxdepth 1 -iname "*.app" -print0)
+  done
+}
+
+while getopts 'thc' opt; do
   case "$opt" in
     t) echo "[*] Getting tcc-related entitlements"
        appPath="$2"
        getEntitlements | grep -e "Getting" -e "tcc" -e "kTCC"
        exit 0;;
     h) usage; exit 0;;
+    c) getCommon; exit 0;;
     *) usage &>2; exit 1;; 
   esac
 done
