33600af509a9f9aea38355c208bd4c3c270c2710
corekore <cristian.radu00@yahoo.com>
2025-08-29 02:02:08 +0300

    running utm vms

---
+++ tools/vm_runner.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+host=$HOST
+vm_dirpath=$VM_DIRPATH
+
+usage() {
+  echo "Usage: ./vm_runner.sh"
+}
+
+vm_runner() {
+  # get a list of vm's that contain "mac" in their name
+  vms=($(utmctl list | awk '{print $3}' | tail -n +2 | grep "mac"))
+  for ((i = 0; i < ${#vms[@]}; i++)); do
+    printf "[*] Starting %s...\n" "${vms[i]}"
+    utmctl start ${vms[i]}
+    sleep 10
+    printf "[*] Collecting data...\n"
+    # using the name from the .ssh config file
+    ./collect_vm_data.sh "vm$((i+1))" "$host" "$vm_dirpath"
+    utmctl stop ${vms[i]}
+    printf "[*] Stopping %s...\n" "${vms[i]}"
+    sleep 10
+  done
+
+}
+if [ $# -eq 0 ]; then
+  vm_runner
+else
+  usage
+fi
